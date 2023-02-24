8868dc92c98f1deea12eae75e36bbede0392a788
dae-wook <wkdrn8860@naver.com>
2023-02-24 15:22:11 +0900

    feat: tiemstamp 작성, post엔티티 작성

--- src/main/java/com/hanghaemini4/jaranghalram/JaranghalramApplication.java
+++ src/main/java/com/hanghaemini4/jaranghalram/JaranghalramApplication.java
@@ -2,8 +2,10 @@ package com.hanghaemini4.jaranghalram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JaranghalramApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/hanghaemini4/jaranghalram/entity/TimeStamped.java
+++ src/main/java/com/hanghaemini4/jaranghalram/entity/TimeStamped.java
@@ -1,4 +1,21 @@
 package com.hanghaemini4.jaranghalram.entity;
 
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
 public class TimeStamped {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime regDt;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime modDt;
 }
