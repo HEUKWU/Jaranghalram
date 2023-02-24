192ed518a6bbf14c992a1eb98d2002643b77ff80
HEUKWU <yoo4068@gmail.com>
2023-02-24 14:47:42 +0900

    Initial commit

---
+++ src/main/java/com/hanghaemini4/jaranghalram/JaranghalramApplication.java
@@ -0,0 +1,13 @@
+package com.hanghaemini4.jaranghalram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaranghalramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaranghalramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghaemini4/jaranghalram/JaranghalramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghaemini4.jaranghalram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaranghalramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
