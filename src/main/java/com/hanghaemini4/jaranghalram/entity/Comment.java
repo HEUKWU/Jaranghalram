9f722a845f13da281eb9c99a17f36170681cdd8f
HEUKWU <yoo4068@gmail.com>
2023-02-24 15:22:36 +0900

    comment 엔티티 설정

--- src/main/java/com/hanghaemini4/jaranghalram/entity/Comment.java
+++ src/main/java/com/hanghaemini4/jaranghalram/entity/Comment.java
@@ -1,4 +1,22 @@
 package com.hanghaemini4.jaranghalram.entity;
 
-public class Comment extends TimeStamped{
+import javax.persistence.*;
+
+public class Comment extends TimeStamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    private String commentContent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
 }
