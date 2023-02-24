0151c5160b53fcf8849d7285952d059bee8a4d50
SEONGHYUN PARK <sparklep519@gmail.com>
2023-02-24 17:31:04 +0900

    PostLike function Update

--- src/main/java/com/hanghaemini4/jaranghalram/controller/PostController.java
+++ src/main/java/com/hanghaemini4/jaranghalram/controller/PostController.java
@@ -1,7 +1,25 @@
 package com.hanghaemini4.jaranghalram.controller;
 
+import com.hanghaemini4.jaranghalram.security.UserDetailsImpl;
+import com.hanghaemini4.jaranghalram.service.PostLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class PostController {
+    private final PostLikeService postLikeService;
+
+    @PostMapping("/like/{postId}")
+    public String likePost(@PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return postLikeService.likePost(postId, userDetails.getUser());
+    }
+
+
+
 }
---
+++ src/main/java/com/hanghaemini4/jaranghalram/service/PostLikeService.java
@@ -0,0 +1,35 @@
+package com.hanghaemini4.jaranghalram.service;
+
+import com.hanghaemini4.jaranghalram.entity.Post;
+import com.hanghaemini4.jaranghalram.entity.PostLike;
+import com.hanghaemini4.jaranghalram.entity.User;
+import com.hanghaemini4.jaranghalram.jwt.JwtUtil;
+import com.hanghaemini4.jaranghalram.repository.PostLIkeRepository;
+import com.hanghaemini4.jaranghalram.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostLikeService {
+    private final PostRepository postRepository;
+    private final PostLIkeRepository postLikeRepository;
+
+    @Transactional
+    public String likePost(Long postId, User user) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new NullPointerException("존재하지 않는 댓글"));
+        if (postLikeRepository.findByBoardIdAndUserId(postId, user.getId()).isPresent()) {
+            post.likeChek((post.getPostLikeCount()) - 1);
+            postLikeRepository.deleteBoardLikeByUserId(user.getId());
+            return "좋아요 취소";
+        }
+        post.likeChek((post.getPostLikeCount()) - 1);
+        PostLike postlike = new PostLike(user, post);
+        postLikeRepository.save(postlike);
+        return "좋아요 성공";
+    }
+}
+//public void likeChek(int count){
+//this.postLikecount = count;
+//}
