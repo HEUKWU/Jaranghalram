b2f3b8786e8937046d4c9a75b83650ce20a243f1
dae-wook <wkdrn8860@naver.com>
2023-02-25 17:03:50 +0900

    fix: 댓글 삭제, 수정 ResponseDto반환

--- src/main/java/com/hanghaemini4/jaranghalram/service/CommentService.java
+++ src/main/java/com/hanghaemini4/jaranghalram/service/CommentService.java
@@ -2,6 +2,7 @@ package com.hanghaemini4.jaranghalram.service;
 
 import com.hanghaemini4.jaranghalram.dto.CommentRequestDto;
 import com.hanghaemini4.jaranghalram.dto.CommentResponseDto;
+import com.hanghaemini4.jaranghalram.dto.ResponseDto;
 import com.hanghaemini4.jaranghalram.entity.Comment;
 import com.hanghaemini4.jaranghalram.entity.Post;
 import com.hanghaemini4.jaranghalram.entity.User;
@@ -26,20 +27,22 @@ public class CommentService {
     }
 
     @Transactional
-    public void update(Long postId, CommentRequestDto requestDto, User user) {
+    public ResponseDto<String> update(Long postId, CommentRequestDto requestDto, User user) {
         Comment comment = commentRepository.findById(postId).orElseThrow(NullPointerException::new);
         if (comment.getUser().getUserName().equals(user.getUserName())) {
             comment.update(requestDto.getContent());
+            return ResponseDto.success("댓글 수정 성공");
         } else {
             throw new IllegalArgumentException("작성자만 수정 가능");
         }
     }
 
     @Transactional
-    public void delete(Long postId, User user) {
+    public ResponseDto<String> delete(Long postId, User user) {
         Comment comment = commentRepository.findById(postId).orElseThrow(NullPointerException::new);
         if (comment.getUser().getUserName().equals(user.getUserName())) {
             commentRepository.deleteById(postId);
+            return ResponseDto.success("댓글 삭제 성공");
         } else {
             throw new IllegalArgumentException("작성자만 삭제 가능");
         }
