b92e91eb6f88c149943105b9b18534f8e68a0d90
HEUKWU <yoo4068@gmail.com>
2023-02-25 21:16:52 +0900

    post, comment 반환값 수정
    Co-authored-by: dae-wook <wkdrn8860@naver.com>
    Co-authored-by: seonghyun519 <sparklep519@gmail.com>
    Co-authored-by: PracticingGit<khgm234@gmail.com>

--- src/main/java/com/hanghaemini4/jaranghalram/controller/CommentController.java
+++ src/main/java/com/hanghaemini4/jaranghalram/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.hanghaemini4.jaranghalram.controller;
 
 import com.hanghaemini4.jaranghalram.dto.CommentRequestDto;
 import com.hanghaemini4.jaranghalram.dto.CommentResponseDto;
+import com.hanghaemini4.jaranghalram.dto.ResponseDto;
 import com.hanghaemini4.jaranghalram.security.UserDetailsImpl;
 import com.hanghaemini4.jaranghalram.service.CommentService;
 import lombok.RequiredArgsConstructor;
@@ -15,17 +16,17 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/api/comment/{postId}")
-    public CommentResponseDto addComment(@PathVariable Long postId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseDto<?> addComment(@PathVariable Long postId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.add(postId, requestDto, userDetails.getUser());
     }
 
     @PutMapping("api/comment/{postId}")
-    public void updateComment(@PathVariable Long postId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        commentService.update(postId, requestDto, userDetails.getUser());
+    public ResponseDto<?> updateComment(@PathVariable Long postId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return commentService.update(postId, requestDto, userDetails.getUser());
     }
 
     @DeleteMapping("api/comment/{postId}")
-    public void deleteComment(@PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        commentService.delete(postId, userDetails.getUser());
+    public ResponseDto<?> deleteComment(@PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return commentService.delete(postId, userDetails.getUser());
     }
 }
--- src/main/java/com/hanghaemini4/jaranghalram/service/CommentService.java
+++ src/main/java/com/hanghaemini4/jaranghalram/service/CommentService.java
@@ -1,7 +1,7 @@
 package com.hanghaemini4.jaranghalram.service;
 
 import com.hanghaemini4.jaranghalram.dto.CommentRequestDto;
-import com.hanghaemini4.jaranghalram.dto.CommentResponseDto;
+import com.hanghaemini4.jaranghalram.dto.ResponseDto;
 import com.hanghaemini4.jaranghalram.entity.Comment;
 import com.hanghaemini4.jaranghalram.entity.Post;
 import com.hanghaemini4.jaranghalram.entity.User;
@@ -19,27 +19,29 @@ public class CommentService {
     private final PostRepository postRepository;
 
     @Transactional
-    public CommentResponseDto add(Long postId, CommentRequestDto requestDto, User user) {
+    public ResponseDto<?> add(Long postId, CommentRequestDto requestDto, User user) {
         Post post = postRepository.findById(postId).orElseThrow(IllegalArgumentException::new);
-        Comment comment = commentRepository.save(new Comment(requestDto.getContent(), post, user));
-        return new CommentResponseDto(comment);
+        commentRepository.save(new Comment(requestDto.getContent(), post, user));
+        return ResponseDto.success("댓글 등록 성공");
     }
 
     @Transactional
-    public void update(Long postId, CommentRequestDto requestDto, User user) {
+    public ResponseDto<?> update(Long postId, CommentRequestDto requestDto, User user) {
         Comment comment = commentRepository.findById(postId).orElseThrow(NullPointerException::new);
         if (comment.getUser().getUserName().equals(user.getUserName())) {
             comment.update(requestDto.getContent());
+            return ResponseDto.success("댓글 수정 성공");
         } else {
             throw new IllegalArgumentException("작성자만 수정 가능");
         }
     }
 
     @Transactional
-    public void delete(Long postId, User user) {
+    public ResponseDto<?> delete(Long postId, User user) {
         Comment comment = commentRepository.findById(postId).orElseThrow(NullPointerException::new);
         if (comment.getUser().getUserName().equals(user.getUserName())) {
             commentRepository.deleteById(postId);
+            return ResponseDto.success("댓글 삭제 성공");
         } else {
             throw new IllegalArgumentException("작성자만 삭제 가능");
         }
