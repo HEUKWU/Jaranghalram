c8d56ab43a00bfa0ff5f5adc98f03d656eebb0e3
dae-wook <wkdrn8860@naver.com>
2023-02-25 21:17:46 +0900

    SecurityConfig H2관련 설정 제거

--- src/main/java/com/hanghaemini4/jaranghalram/config/WebSecurityConfig.java
+++ src/main/java/com/hanghaemini4/jaranghalram/config/WebSecurityConfig.java
@@ -37,10 +37,10 @@ public class WebSecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         // h2-console 사용 및 resources 접근 허용 설정 //필터 거치지 않고 패스
         return (web) -> web.ignoring()
-                .requestMatchers(PathRequest.toH2Console())
+//                .requestMatchers(PathRequest.toH2Console())
                 .antMatchers("/api/auth/**", "/",
-                        "/v2/api-docs", "/swagger-resources/**", "/swagger-ui/index.html", "/swagger-ui.html", "/webjars/**", "/swagger/**",   // swagger
-                        "/h2-console/**")
+                        "/v2/api-docs", "/swagger-resources/**", "/swagger-ui/index.html", "/swagger-ui.html", "/webjars/**", "/swagger/**"   // swagger
+                        )
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
