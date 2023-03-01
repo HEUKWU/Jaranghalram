2effb0296b1d563327400ae15a7579304e9e0250
DESKTOP-JQDL6G8\theCHAIN <khgm234@gmail.com>
2023-03-01 17:34:56 +0900

    좋아요 수 GET API 추가
    좋아요 수만 따로 GET API추가

--- src/main/java/com/hanghaemini4/jaranghalram/controller/PostLikeController.java
+++ src/main/java/com/hanghaemini4/jaranghalram/controller/PostLikeController.java
@@ -5,10 +5,7 @@ import com.hanghaemini4.jaranghalram.security.UserDetailsImpl;
 import com.hanghaemini4.jaranghalram.service.PostLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostLikeController {
     private final PostLikeService postLikeService;
 
+    @GetMapping("/like-counts/{postId}")
+    public ResponseDto<Integer> likeCountsCheck(@PathVariable Long postId) {
+        return postLikeService.likeCountsCheck(postId);
+    }
     @PostMapping("/like-counts/{postId}")
     public ResponseDto<Boolean> likePost(@PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return postLikeService.likePost(postId, userDetails.getUser());
